cdb7208dbe1dd739cb84ec9fd37f492d121da9ad
yaraAawad <awadyara2000@gmail.com>
2023-12-31 21:28:43 +0300

    ReactJS
    initial

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "56d2870a57mshc26d7622883ba8dp1225aejsn82816279af48",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8aa26bfcda9e0a32957300d0baf38a6e"; 
